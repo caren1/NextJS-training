87fe2403ed323a30945db464e63bf12cf6fff35f
caren1 <czarnocki.w@wp.pl>
2021-01-28 17:56:09 +0100

    refactor + layout and css modules

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Webdev news</title>
+        <meta name="keywords" content="web development, programming" />
+      </Head>
+      <h1>Welcome to Next!</h1>
+    </div>
+  );
+}
