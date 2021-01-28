2c233ee2a9ab81e9567536645233bceb5a136f1c
caren1 <czarnocki.w@wp.pl>
2021-01-28 18:38:46 +0100

    getStaticPaths()

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from 'next/link'
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 const article = ({ article }) => {
@@ -7,18 +7,18 @@ const article = ({ article }) => {
   // const router = useRouter();
   // const { id } = router.query;
 
-  return(
-  <>
-    <h1>{article.title}</h1>
-    <p>{article.body}</p>
-    <br />
-    <Link href="/">Go back</Link>
-  </>
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href="/">Go back</Link>
+    </>
   );
 };
 
 // to both methods we can pass the context that will get an access to the url and id
-export const getServerSideProps = async (context) => {
+export const getStaticProps = async (context) => {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
   );
@@ -29,6 +29,20 @@ export const getServerSideProps = async (context) => {
     props: {
       article,
     },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
+  const articles = await res.json();
+
+  const ids = articles.map((article) => article.id);
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+  return {
+    paths,
+    fallback: false
   };
 };
 
