83ccb9b4e79973d7e4c98d955ed6921dc9c15ec9
caren1 <czarnocki.w@wp.pl>
2021-01-28 19:02:47 +0100

    custom meta component

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,10 @@
 import React from "react";
-import Head from "next/head";
+import Meta from '../components/Meta'
 
 const About = () => {
   return (
     <div>
-      <Head>
-        <title>About</title>
-        <meta name="keywords" content="web development, programming" />
-      </Head>
+      <Meta title="About"/>
       <h1>About</h1>
     </div>
   );
