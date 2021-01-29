52ed736d779c6c5f6a522f872a62d9d2573c2898
caren1 <czarnocki.w@wp.pl>
2021-01-29 13:36:37 +0100

    routes and pages

---
+++ ninjalist-next-course/pages/about.js
@@ -0,0 +1,24 @@
+const about = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+        maiores aliquam optio dolor. Excepturi inventore, quia consequatur eaque
+        voluptatem expedita?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+        maiores aliquam optio dolor. Excepturi inventore, quia consequatur eaque
+        voluptatem expedita?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+        maiores aliquam optio dolor. Excepturi inventore, quia consequatur eaque
+        voluptatem expedita?
+      </p>
+    </div>
+  );
+};
+
+export default about;
--- ninjalist-next-course/pages/index.js
+++ ninjalist-next-course/pages/index.js
@@ -1,10 +1,16 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Homepage</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
+        doloribus similique unde minima maxime quasi numquam nostrum pariatur
+        vitae recusandae mollitia quod libero explicabo, ea id, dolore a facere
+        odit.
+      </p>
     </div>
-  )
+  );
 }
