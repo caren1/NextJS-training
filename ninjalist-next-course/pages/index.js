459a318e7bf221013bfc612e371494f7bc5ebe55
caren1 <czarnocki.w@wp.pl>
2021-01-29 14:05:08 +0100

    adding styles

--- ninjalist-next-course/pages/index.js
+++ ninjalist-next-course/pages/index.js
@@ -1,21 +1,17 @@
-import Head from "next/head";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
 import Link from 'next/link'
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      
-      <h1>Homepage</h1>
-      <p>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
         doloribus similique unde minima maxime quasi numquam nostrum pariatur
         vitae recusandae mollitia quod libero explicabo, ea id, dolore a facere
         odit.
       </p>
-      <Link href='/ninjas'><a> See Ninja Listing</a></Link>
+      <Link href='/ninjas'><a className={styles.btn}> See Ninja Listing</a></Link>
       
     </div>
   );
