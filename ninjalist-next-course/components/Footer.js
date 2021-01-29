155adb668c38076550093ebaac580b0204267c40
caren1 <czarnocki.w@wp.pl>
2021-01-29 13:40:58 +0100

    adding other components

---
+++ ninjalist-next-course/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <p>Copyright 2021 - Ninja List</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
--- ninjalist-next-course/pages/index.js
+++ ninjalist-next-course/pages/index.js
@@ -1,9 +1,12 @@
 import Head from "next/head";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
@@ -11,6 +14,7 @@ export default function Home() {
         vitae recusandae mollitia quod libero explicabo, ea id, dolore a facere
         odit.
       </p>
+      <Footer />
     </div>
   );
 }
