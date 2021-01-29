0c1a513860019c90600e3de2c289944897df7c21
caren1 <czarnocki.w@wp.pl>
2021-01-29 14:09:19 +0100

    404 page

---
+++ ninjalist-next-course/pages/404.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+
+const NotFound = () => {
+    return (
+        <div className="not-found">
+            <h1>Whoops!</h1>
+            <h2>This page cannot be found.</h2>
+            <p>Go back to the <Link href="/"><a>Homepage</a></Link></p>
+        </div>
+    )
+}
+
+export default NotFound;
