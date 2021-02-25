bf67cac15d26e553629fafa6127fc1451c200a5c
caren1 <czarnocki.w@wp.pl>
2021-02-25 14:08:41 +0100

    redirection

--- ninjalist-next-course/pages/404.js
+++ ninjalist-next-course/pages/404.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 
 const NotFound = () => {
+
+    const router = useRouter();
+    
+    useEffect(() => {
+        setTimeout(() => {
+            // router.go()
+            router.push('/')
+        }, 3000);
+    }, [])
+
     return (
         <div className="not-found">
             <h1>Whoops!</h1>
